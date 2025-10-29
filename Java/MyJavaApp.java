1332bc671ee66a260010aade65840298bdb8eb96
jagasia <rjagadeeswaran@Yahoo.com>
2025-10-29 10:53:07 +0530

    29Oct 10:53

---
+++ Java/MyJavaApp.java
@@ -0,0 +1,9 @@
+  public class MyJavaApp {
+            public static void main(String[] args) {
+                System.out.println("Hello from MyJavaApp!");
+                int a = 10;
+                int b = 25;
+                int sum = a + b;
+                System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+            }
+        }
