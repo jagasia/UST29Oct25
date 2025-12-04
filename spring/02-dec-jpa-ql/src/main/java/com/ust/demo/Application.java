6f5f7b31e48ad8bff06e1a49fb3778a9294b6ac8
jagasia <rjagadeeswaran@Yahoo.com>
2025-12-04 16:53:07 +0530

    04Dec 16:52

--- spring/02-dec-jpa-ql/src/main/java/com/ust/demo/Application.java
+++ spring/02-dec-jpa-ql/src/main/java/com/ust/demo/Application.java
@@ -1,13 +1,39 @@
 package com.ust.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ust.demo.model.Product;
+import com.ust.demo.service.ProductService;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	
+	private ProductService ps;
+	
+	public Application(ProductService ps) {
+		this.ps=ps;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String ...args) {
+		Product product=new Product();
+		product.setName("Water bottle");
+		product.setCategory("Fancy");
+		product.setPrice(100.0);
+		
+		Product res = ps.create(product);
+		if(res.getId()!=null) {
+			//success
+			System.out.println("Success");
+		}else {
+			System.out.println("Failure");
+		}
+		
+	}
 }
